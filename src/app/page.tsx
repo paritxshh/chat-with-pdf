c6237119b6d0b64c044f69cf214c16b228e5875c
Paritosh <paritoshmthr0106@gmail.com>
2025-09-18 21:04:06 +0530

    Enhance styling by applying Poppins font to various text elements and adding gap between components for improved layout consistency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,10 +11,10 @@ export default async function Home() {
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <div className="flex flex-col items-center text-center">
+        <div className="flex flex-col items-center text-center gap-y-2">
 
           <div className="flex items-center">
-            <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
+            <h1 className="mr-3 text-5xl font-semibold font-poppins">Chat with any PDF</h1>
             <UserButton />
           </div>
 
@@ -22,12 +22,12 @@ export default async function Home() {
             {isAuth && <Button>Go to Chats</Button>}
           </div>
 
-          <p className="max-w-xl mt-1 text-lg text-slate-600">Join millions of students, researchers and professionals to instantly answer questions and understand research with AI.</p>
+          <p className="max-w-xl mt-1 text-lg text-slate-600 font-poppins">Join millions of students, researchers and professionals to instantly answer questions and understand research with AI.</p>
 
           <div className="w-full mt-4">
             {isAuth ? (<FileUpload />):(
               <Link href="/sign-in">
-                <Button>Login to get Started !!<LogInIcon className="w-4 h-4 ml-2" /></Button>
+                <Button className="font-poppins">Login to get Started !!<LogInIcon className="w-4 h-4 ml-2" /></Button>
               </Link>
             )}
           </div>
